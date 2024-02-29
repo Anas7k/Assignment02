90aa80de73e27ef1c9c6a60e90b1b5320447ad00
Anas Ahmed <117765449+Anas7k@users.noreply.github.com>
2024-02-28 17:38:47 -0800

    Update 04-grade-assigner.js

--- js/04-grade-assigner.js
+++ js/04-grade-assigner.js
@@ -1,24 +1,5 @@
 let score = parseInt(prompt("Enter your score (between 1 and 100):"));
 
-if (score >= 1 && score <= 100) {
-    if (score >= 90) {
-        console.log("You received an A");
-    } else if (score >= 80) {
-        console.log("You received a B");
-    } else if (score >= 70) {
-        console.log("You received a C");
-    } else if (score >= 60) {
-        console.log("You received a D");
-    } else {
-        console.log("You received an F");
-    }
-} else {
-    console.log("Only numbers between 1 and 100 are accepted.");
-}
-
-/*
-let score = parseInt(prompt("Enter your score (between 1 and 100):"));
-
 switch (true) {
     case score >= 90 && score <= 100:
         console.log("You received an A");
@@ -38,5 +19,3 @@ switch (true) {
     default:
         console.log("Only numbers between 1 and 100 are accepted.");
         break;
-}
-*/
